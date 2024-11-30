d9fa2a52cfc03414957af09c7141c640d1438d57
sharady30 <123531467+sharady30@users.noreply.github.com>
2024-11-30 12:36:00 +0530

    Update settings.py

--- curaams_backend/settings.py
+++ curaams_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=5yciw=5fiii*g^l(#3-#)=appl-pkz7w0l%v5*kkfp051uc1b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
