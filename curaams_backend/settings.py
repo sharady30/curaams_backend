4f206a1e6f2a2b96b4c95297197ca66a372c4945
sharady30 <123531467+sharady30@users.noreply.github.com>
2024-11-30 12:53:49 +0530

    Update settings.py

--- curaams_backend/settings.py
+++ curaams_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=5yciw=5fiii*g^l(#3-#)=appl-pkz7w0l%v5*kkfp051uc1b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app"]
 
